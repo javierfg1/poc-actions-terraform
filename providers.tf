9cae30c6b5e64b9c32a62d6e27b3888928b75b6e
Ubuntu <ubuntu@ip-10-0-101-251.ec2.internal>
2023-12-18 18:29:38 +0000

    first commit

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "tf-remote-state20231215182122478600000001"
+    key    = "my-vpc"
+    dynamodb_table = "tf-remote-state-lock"
+    region = "us-east-1"
+  }
+ 
+
+  required_version = ">= 1.1.5"
+}
+
+provider "aws" {
+  region = var.region
+} 
