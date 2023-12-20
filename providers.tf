505d7b75453afe5abc6b9bf775fb400b712ffdc0
Ubuntu <ubuntu@ip-10-0-101-251.ec2.internal>
2023-12-20 11:37:42 +0000

    0001

--- providers.tf
+++ providers.tf
@@ -7,16 +7,16 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "tf-remote-state20231220080350663400000001"
-    key    = "my-vpc"
+    bucket         = "tf-remote-state20231220080350663400000001"
+    key            = "my-vpc"
     dynamodb_table = "tf-remote-state-lock"
-    region = "us-east-1"
+    region         = "us-east-1"
   }
- 
+
 
   required_version = ">= 1.1.5"
 }
 
 provider "aws" {
   region = var.region
-} 
+}
